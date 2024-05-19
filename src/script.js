be378ff4d4ba8354316aa090a4d07874504e12d1
lmos0 <luismarcio.os@outlook.com>
2024-05-18 22:04:52 -0300

    inclusao de um backend prototipo

--- src/script.js
+++ src/script.js
@@ -2,15 +2,35 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var resultado = document.getElementById('resultado')
     let formulario = document.getElementById('formulario')
+    let nome = document.getElementById('nome')
+    let email = document.getElementById('email')
 
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
     calcularConta()
+    salvarLocalStorage()
 
 })
 
+function salvarLocalStorage(){
+    let data ={
+        nome: nome.value,
+        email: email.value,
+        valorConta: resultado.value,
+        resultado: resultado.value
+    }
 
+    localStorage.setItem('data', JSON.stringify(data))
+    
+    alert('Dados salvos com sucesso')
+
+    
+}
+
+function teste(){
+    alert('teste')
+}
 function calcularConta(){
     let conta = document.getElementById('valorConta')
     let valorConta = Number(conta.value)
--- src/v1/script.js
+++ src/v1/script.js
@@ -2,13 +2,35 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var resultado = document.getElementById('resultado')
     let formulario = document.getElementById('formulario')
+    let nome = document.getElementById('nome')
+    let email = document.getElementById('email')
+    let dataList = document.getElementById('dataList')
 
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
     calcularConta()
+    salvarLocalStorage()
 
 })
+
+function salvarLocalStorage(){
+    let potenciasClientes = JSON.parse(localStorage.getItem('data')) || []
+
+    let data ={
+        nome: nome.value,
+        email: email.value,
+        valorConta: valorConta.value,
+        resultado: resultado.value
+    }
+    potenciasClientes.push(data)
+
+    localStorage.setItem('data', JSON.stringify(potenciasClientes))
+    
+    alert('Dados salvos com sucesso')
+
+    
+}
 
 
 function calcularConta(){
@@ -22,4 +44,24 @@ function calcularConta(){
 resultado.value = `O investimento será de R$ ${total.toFixed(2)}`
 }
 
+
+
 })
+
+function displayData() {
+    let data = JSON.parse(localStorage.getItem('data')) || []
+
+    dataList.innerHTML = ''
+
+    data.forEach(item => {
+        const li = document.createElement('li')
+        li.textContent = `Nome: ${item.nome}, Email: ${item.email}, Valor da Conta: ${item.valorConta}, Resultado: ${item.resultado}`
+        dataList.appendChild(li)
+    });
+}
+
+function clearData() {
+    localStorage.clear()
+    alert('Todos os dados foram excluídos do LocalStorage')
+    dataList.innerHTML = ''
+}
