fd16dcfe8341ca7b3871313fc776bc6162caa025
RicardoGNog <149633605+RicardoGNog@users.noreply.github.com>
2024-06-20 13:09:12 -0300

    Subindo a versão 2, com o layout novo implementado

---
+++ src/v2/co2-script.js
@@ -0,0 +1,21 @@
+function calculateCO2() {
+    var energyInput = document.getElementById("energyConsumption").value;
+
+    if (energyInput === "" || isNaN(energyInput)) {
+        alert("Por favor, digite um valor válido.");
+        return;
+    }
+
+    var energyConsumption = parseFloat(energyInput);
+
+    var coalEmission = energyConsumption * 1;
+    var oilEmission = energyConsumption * 0.8;
+    var gasEmission = energyConsumption * 0.5;
+    var hydroEmission = energyConsumption * 0.1;
+
+    document.getElementById("CO2results").innerHTML = 
+        "<p>Se proveniente de carvão: " + coalEmission.toFixed(2) + " kg de CO².</p>" +
+        "<p>Se proveniente de petróleo: " + oilEmission.toFixed(2) + " kg de CO².</p>" +
+        "<p>Se proveniente de gás natural: " + gasEmission.toFixed(2) + " kg de CO².</p>" +
+        "<p>Se proveniente de energia hidrelétrica: " + hydroEmission.toFixed(2) + " kg de CO².</p>";
+}
